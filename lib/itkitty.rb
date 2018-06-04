d6e95d894aab2cccd843cd20b7043526cd049c72
Chewsterchew <chew@chew.pw>
2018-06-03 21:15:53 -0500

    track jOyns

--- lib/itkitty.rb
+++ lib/itkitty.rb
@@ -34,6 +34,26 @@ end
 Bot.server_delete do |event|
 end
 
+Bot.member_join do |event|
+  bot.channel(452_890_393_286_148_097).send_embed do |embed|
+    embed.title = 'User Joined the Server!'
+    embed.colour = 0xd084
+    embed.description = "Please welcome #{event.user.mention} to the server!"
+
+    embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "Member Count: #{event.server.member_count}")
+  end
+end
+
+Bot.member_leave do |event|
+  bot.channel(452_890_393_286_148_097).send_embed do |embed|
+    embed.title = 'User Left the Server!'
+    embed.colour = 0xd084
+    embed.description = "#{event.user.distinct} left! RIP :("
+
+    embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "Member Count: #{event.server.member_count}")
+  end
+end
+
 Bot.ready do |_event|
   Bot.game = 'mew ;3'
 end
